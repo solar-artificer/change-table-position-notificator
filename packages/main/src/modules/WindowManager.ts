2dd3fc30f27d945e12475cdb94349e90e313f1a6
Anton Zhluktarov <anton.zhluktarov@gmail.com>
2025-09-15 00:19:35 +0300

    Befor XState overhaul

--- packages/main/src/modules/WindowManager.ts
+++ packages/main/src/modules/WindowManager.ts
@@ -1,15 +1,18 @@
 import type {AppModule} from '../AppModule.js';
 import {ModuleContext} from '../ModuleContext.js';
-import {BrowserWindow, ipcMain, screen} from 'electron';
+import {BrowserWindow, ipcMain, screen, Display, globalShortcut} from 'electron';
 import type {AppInitConfig} from '../AppInitConfig.js';
 import FullScreenDisplayChecker from '../fullscreen-display-checker/FullScreenDisplayChecker.js';
-import Display = Electron.Display;
+import Play from 'play-sound';
 
 class WindowManager implements AppModule {
   readonly #preload: {path: string};
   readonly #renderer: {path: string} | URL;
   readonly #popup: {path: string} | URL;
   readonly #openDevTools;
+
+  #mainWindow: BrowserWindow | null = null;
+  #popupWindow: BrowserWindow | null = null;
 
   constructor({initConfig, openDevTools = false}: {initConfig: AppInitConfig, openDevTools?: boolean}) {
     this.#preload = initConfig.preload;
@@ -20,11 +23,27 @@ class WindowManager implements AppModule {
 
   async enable({app}: ModuleContext): Promise<void> {
     await app.whenReady();
+
+    globalShortcut.register('F13', () => {
+      this.#popupWindow!.hide();
+      this.#mainWindow!.webContents.send('restart-timer');
+    })
+
     await this.restoreOrCreateWindow(true);
     app.on('second-instance', () => this.restoreOrCreateWindow(true));
     app.on('activate', () => this.restoreOrCreateWindow(true));
 
-    ipcMain.handle('spawn-notification', async () => {
+    ipcMain.handle('minimize-window', async () => {
+      this.#mainWindow!.minimize();
+    });
+    ipcMain.handle('maximize-window', async () => {
+      this.#mainWindow!.maximize();
+    });
+    ipcMain.handle('close-window', async () => {
+      this.#mainWindow!.close();
+    });
+
+    ipcMain.handle('timer-rundown', async () => {
       const displaysToFullScreenApps: object = new FullScreenDisplayChecker().getDisplaysAndFullScreenApps();
       const displays = screen.getAllDisplays();
 
@@ -93,38 +112,40 @@ class WindowManager implements AppModule {
       const windowX = targetDisplay.workArea.x + targetDisplay.workArea.width - windowWidth - 10;
       const windowY = targetDisplay.workArea.y + targetDisplay.workArea.height - windowHeight - 10;
 
-      const browserWindow = new BrowserWindow({
-        x: windowX,
-        y: windowY,
+      if (this.#popupWindow === null || this.#popupWindow === undefined) {
+        this.#popupWindow = new BrowserWindow({
+          x: windowX,
+          y: windowY,
 
-        height: windowHeight,
-        width: windowWidth,
+          height: windowHeight,
+          width: windowWidth,
 
-        show: false, // Use the 'ready-to-show' event to show the instantiated BrowserWindow.
-        webPreferences: {
-          nodeIntegration: false,
-          contextIsolation: true,
-          sandbox: false, // Sandbox disabled because the demo of preload script depend on the Node.js api
-          webviewTag: false, // The webview tag is not recommended. Consider alternatives like an iframe or Electron's BrowserView. @see https://www.electronjs.org/docs/latest/api/webview-tag#warning
-          preload: this.#preload.path,
-        },
-        titleBarStyle: "hidden",
-        autoHideMenuBar: true
-      });
+          show: false, // Use the 'ready-to-show' event to show the instantiated BrowserWindow.
+          webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: true,
+            sandbox: false, // Sandbox disabled because the demo of preload script depend on the Node.js api
+            webviewTag: false, // The webview tag is not recommended. Consider alternatives like an iframe or Electron's BrowserView. @see https://www.electronjs.org/docs/latest/api/webview-tag#warning
+            preload: this.#preload.path,
+          },
+          titleBarStyle: "hidden",
+          autoHideMenuBar: true
+        });
 
-      if (this.#popup instanceof URL) {
-        await browserWindow.loadURL(this.#popup.href);
+        if (this.#popup instanceof URL) {
+          await this.#popupWindow.loadURL(this.#popup.href);
+        } else {
+          await this.#popupWindow.loadFile(this.#popup.path);
+        }
+
+        if (this.#openDevTools) {
+          this.#popupWindow?.webContents.openDevTools();
+        }
+        this.#popupWindow.show();
       } else {
-        await browserWindow.loadFile(this.#popup.path);
+        this.#popupWindow.show();
+        this.#popupWindow.webContents.send('play-timer-sound');
       }
-
-      browserWindow.show();
-
-      if (this.#openDevTools) {
-        //browserWindow?.webContents.openDevTools();
-      }
-
-      browserWindow.focus();
     });
   }
 
@@ -157,6 +178,8 @@ class WindowManager implements AppModule {
     if (window === undefined) {
       window = await this.createWindow();
     }
+
+    this.#mainWindow = window;
 
     if (!show) {
       return window;
